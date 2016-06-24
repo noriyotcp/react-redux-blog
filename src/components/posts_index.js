c24ce0b81cdde8092562faedfcdbd5581b4afe48
noriyotcp <noriyo.akita@gmail.com>
2016-06-24 21:28:51 +0900

    Rename post_index.js to posts_index.js

---
+++ src/components/posts_index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class PostsIndex extends Component {
+  componentWillMount() {
+    console.log('this would be a good time to call an action.');
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default PostsIndex;
