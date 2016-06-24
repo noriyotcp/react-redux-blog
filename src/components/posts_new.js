9de1a76ec6cdb7e1500e236fadf67593fd3558f8
noriyotcp <noriyo.akita@gmail.com>
2016-06-24 23:31:58 +0900

    Add components/posts_new.js

---
+++ src/components/posts_new.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class PostsNew extends Component {
+  render() {
+    return (
+      <div>Create Form</div>
+    );
+  }
+}
+
+export default PostsNew;
