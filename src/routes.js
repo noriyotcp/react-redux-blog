743d5d3d63a3e58adddd87ba91f042e8662a23af
noriyotcp <noriyo.akita@gmail.com>
2016-06-24 20:37:01 +0900

    Remove Greeting component
    Because it was just a test

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,8 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostIndex from './components/post_index';
 
-const Greeting = () => {
-  return <div>Hey there!</div>;
-};
-
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostIndex} />
-    <Route path="greet" component={Greeting} />
-    <Route path="greet2" component={Greeting} />
-    <Route path="greet3" component={Greeting} />
   </Route>
 );
