07514d71ca9a853312374eaebeae99976aff732e
noriyotcp <noriyo.akita@gmail.com>
2016-06-25 00:21:13 +0900

    (components/posts_new) Add handleSubmit and fields as props to the form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,22 +3,23 @@ import { reduxForm} from 'redux-form';
 
 export class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create Form</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
         </div>
 
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} />
         </div>
 
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content} />
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
