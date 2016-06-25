c97192a74a872322d2775d5c81a359902dbbf967
noriyotcp <noriyo.akita@gmail.com>
2016-06-25 21:03:36 +0900

    (components/posts_new) Define validate() function and error messages for fields(when values do not exist)

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,16 +11,25 @@ export class PostsNew extends Component {
         <div className="form-group">
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
         <div className="form-group">
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
         <div className="form-group">
           <label>Content</label>
           <textarea className="form-control" {...content} />
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -29,10 +38,27 @@ export class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter the title'
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter categories'
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content'
+  }
+
+  return errors;
+}
+
 // connect: 1st argument is mapStateToProps, 2nd argument is mapDispatchToProps
 // reduxForm: 1st argument is from config, 2nd argument is mapStateToProps, 3rd argument is mapDispatchToProps
 
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
