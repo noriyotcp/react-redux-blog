6c70efb3b1b7b74ea4921190cbbe7415394cafc2
noriyotcp <noriyo.akita@gmail.com>
2016-06-26 22:44:57 +0900

    Enable to redirect to index after creating a post is succeed

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm} from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 export class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // Blog porst has been created, navigate the user to index
+        // We navigate by calling this.context.router.push with the
+        // new path to navigate to.
+        this.context.router.push('/');
+      });
+  }
+
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
+
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create Form</h3>
 
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
