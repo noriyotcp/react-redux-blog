92c0d6d1826f4500125dfaef7647891aff2a7aa9
noriyotcp <noriyo.akita@gmail.com>
2016-06-26 23:57:13 +0900

    (components/posts_show) Call fetchPost() with params id when PostsShow component is mounted

---
+++ src/components/posts_show.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
+
+export class PostsShow extends Component {
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
+  render() {
+    return (
+      <div> Show post {this.props.params.id}</div>
+    );
+  }
+}
+
+export default connect(null, { fetchPost })(PostsShow);
