c82f7127dd5deb021617edf6d67df7135fdb7b38
noriyotcp <noriyo.akita@gmail.com>
2016-06-27 15:01:00 +0900

    Move API key to api_keys.js from actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { API_KEY } from '../../api_keys';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
@@ -6,7 +7,6 @@ export const FETCH_POST = 'FETCH_POST';
 export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=fjahflajhfafhaf';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
